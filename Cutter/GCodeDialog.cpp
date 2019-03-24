f062f0fa459eda8d1088ab14c6de9e108e0e72db
rbp28668 <bruce.porteous@ntlworld.com>
2019-03-24 21:06:57 +0000

    Added <assert.h> to cutter files for 32 bit build

--- Cutter/GCodeDialog.cpp
+++ Cutter/GCodeDialog.cpp
@@ -18,6 +18,7 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 //
 
 #include "stdafx.h"
+#include <assert.h>
 #include "GCodeDialog.h"
 #include "afxdialogex.h"
 #include "resource.h"
